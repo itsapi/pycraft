00251d73cf1c915cc8b331a76bd61cbf6f2144eb
Geraint White <geraintwhite@gmail.com>
2014-04-18 02:35:02 +0100

    Added terrain generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from time import sleep
+from time import time
+import random
 
 from colors import colorStr, BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
 from console import WIDTH, HEIGHT
@@ -9,17 +10,13 @@ from nbinput import NonBlockingInput
 def load_map(filename):
     map_obj = json.load(open(filename))
     
-    name = map_obj['meta']['name']
-    center = map_obj['meta']['center']
+    meta = map_obj['meta']
     map_ = map_obj['slices']
 
-    return name, center, map_
+    return meta, map_
 
 
 def move_map(map_, left_edge, right_edge):
-    if left_edge < 0 or right_edge >= len(map_):
-        return False
-
     slices = {}
     for pos in range(left_edge, right_edge):
         pos = str(pos)
@@ -41,6 +38,24 @@ def render_map(map_, blocks):
     print('\n'.join(''.join(blocks[pixel] for pixel in row) for row in map_))
 
 
+def slice_height(seed, pos, max_hill, ground_height):
+    slice_height_ = ground_height
+    for x in range(pos - max_hill, pos + max_hill):
+        random.seed(seed + str(x))
+        if random.randint(0, 100) < 15:
+            hill_height = ground_height + random.randint(0, max_hill) - abs(pos-x)
+            if hill_height > slice_height_:
+                slice_height_ = hill_height
+
+    return slice_height_
+
+
+def gen_slice(seed, pos, height, max_hill, ground_height):
+    slice_height_ = slice_height(seed, pos, max_hill, ground_height)
+    slice_ = [' '] * (height - slice_height_) + ['-'] + ['#'] * (slice_height_ - 1)
+
+    return slice_
+
 def gen_blocks():
     return {
         ' ': ' ', # Air
@@ -55,17 +70,48 @@ def gen_blocks():
         'o': colorStr('o', CYAN) # Diamond
     }
 
+
+def get_pos_d(char):
+    if char == ',':
+        return -1
+    if char == '.':
+        return 1
+    return 0
+
+
 def main():
     blocks = gen_blocks()
-    name, center, map_ = load_map('map.blk')
-    pos = (0, 0)
+    meta, map_ = load_map('map.blk')
 
+    pos = meta['center']
+    width = 20
+
+    FPS = 20
+    SEED = meta['seed']
+    HEIGHT = len(map_['0'])
+    MAX_HILL = 5
+    GROUND_HEIGHT = 13
+
+    last_out = time()
     with NonBlockingInput() as nbi:
         while True:
 
-            chunk = move_map(map_, 0, 40)
-            render_map(chunk, blocks)
-            sleep(0.1)
+            pos += get_pos_d(nbi.char())
+
+            edge = False
+            left_edge, right_edge = pos - int(width / 2), pos + int(width / 2)
+            if left_edge < 0:
+                edge = left_edge
+            if right_edge > len(map_) - 2:
+                edge = right_edge
+            if edge:
+                map_[str(edge)] = gen_slice(SEED, edge, HEIGHT, MAX_HILL, GROUND_HEIGHT)
+
+            chunk = move_map(map_, left_edge, right_edge)
+
+            if time() > last_out + (1 / FPS):
+                last_out = time()
+                render_map(chunk, blocks)
 
 if __name__ == '__main__':
     main()
