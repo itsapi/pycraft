6bfc1cfc259f1e878c7152f1f737c95a55427e07
Geraint White <geraintwhite@gmail.com>
2014-04-20 18:42:19 +0100

    Refactor main.py

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
 
         x = meta['center']
         y = 1
+        dx = 0
+        dy = 0
+        dt = 0
+        df = 0
         width = 40
         FPS = 10
         TPS = 10
@@ -29,6 +33,7 @@ def main():
         inp = None
         jump = 0
         new_slices = {}
+        alive = True
 
         # Game loop
         game = True
@@ -43,35 +48,43 @@ def main():
                 for pos in slice_list:
                     new_slices[str(pos)] = terrain.gen_slice(pos, meta)
                     map_[str(pos)] = new_slices[str(pos)]
-                    redraw = True
 
                 # Save new terrain to file
                 if new_slices:
                     saves.save_map(save, new_slices)
                     new_slices = {}
+                    redraw = True
 
                 # Moving view
                 if not edges == old_edges:
-                    redraw = True
-                    old_edges = edges
                     view = terrain.move_map(map_, edges)
+                    old_edges = edges
+                    redraw = True
 
                 # Draw view
                 if redraw and time() >= 1/FPS + last_out:
+                    df = 1
                     redraw = False
                     last_out = time()
                     terrain.render_map(view, int(width / 2), y, blocks)
-
-                # Increase tick
-                if time() >= (1/TPS) + last_tick:
-                    dt = 1
-                    tick += dt
-                    last_tick = time()
                 else:
-                    dt = 0
+                    df = 0
 
-                # Player falls when no block below it
-                if dt and not blocks[map_[str(x)][y+1]][1]:
+                # Respawn player if dead
+                if not alive and df:
+                    alive = True
+                    x, y = player.respawn(meta)
+
+                # If no block below, kill player
+                try:
+                    block = map_[str(x)][y+1]
+                    below_solid = terrain.is_solid(blocks, block)
+                except IndexError:
+                    below_solid = False
+                    alive = False
+
+                # Player falls when no solid block below it
+                if dt and not below_solid:
                     if jump > 0:
                         jump -= 1
                     else:
@@ -83,17 +96,17 @@ def main():
 
                 inp = char if char in 'wWaAdDfF' else None
 
-                if time() >= (1/TPS) + last_inp:
+                if time() >= (1/TPS) + last_inp and alive:
                     if inp:
-                        dx, dy, jump = get_pos_delta(
+                        dx, dy, jump = player.get_pos_delta(
                             str(inp), map_, x, y, blocks, jump)
                         y += dy
                         x += dx
 
-                        new_slices = break_block(str(inp), map_, x, y, blocks)
+                        new_slices = player.break_block(str(inp), map_, x, y, blocks)
                         map_.update(new_slices)
 
-                        if dx or dy or break_block:
+                        if dx or dy:
                             redraw = True
                         last_inp = time()
                         inp = None
@@ -103,6 +116,14 @@ def main():
                     redraw = True
                     if ui.pause() == 'exit':
                         game = False
+
+                # Increase tick
+                if time() >= (1/TPS) + last_tick:
+                    dt = 1
+                    tick += dt
+                    last_tick = time()
+                else:
+                    dt = 0
 
 
 if __name__ == '__main__':
