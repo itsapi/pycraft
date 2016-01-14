14ac704879ae65503b877c69f52d606efce8a132
Oliver Faircliff <faircliff.oliver@gmail.com>
2016-01-14 00:31:54 +0000

    Remove old terrain gen functions

--- terrain.py
+++ terrain.py
@@ -29,147 +29,26 @@ def move_map(map_, edges):
     return slices
 
 
-def slice_height(pos, meta):
-    slice_height_ = world_gen['ground_height']
+def detect_edges(map_, edges):
+    slices = []
+    for pos in range(*edges):
+        if not str(pos) in map_:
+            slices.append(pos)
 
-    # Check surrounding slices for a hill with min gradient
-    for x in range(pos - world_gen['max_hill'] * world_gen['min_grad'],
-                   pos + world_gen['max_hill'] * world_gen['min_grad']):
-        # Set seed for random numbers based on position
-        random.seed(str(meta['seed']) + str(x) + 'hill')
-
-        # Generate a hill with a 5% chance
-        if random.random() <= 0.05:
-
-            # Get gradient for left, or right side of hill
-            gradient_l = random.randint(1, world_gen['min_grad'])
-            gradient_r = random.randint(1, world_gen['min_grad'])
-
-            gradient = gradient_r if x < pos else gradient_l
-
-            # Height is distance from hill with gradient
-            d_hill_height = abs(pos-x) / gradient
-
-            # Cut off anything that would not be a part of the hill assuming
-            #   flat ground.
-            if d_hill_height < world_gen['max_hill']:
-
-                hill_height = (world_gen['ground_height'] +
-                    random.randint(0, world_gen['max_hill']) - d_hill_height)
-                # Make top of hill flat
-                hill_height -= 1 if pos == x else 0
-
-                slice_height_ = max(slice_height_, hill_height)
-
-    return int(slice_height_)
+    return slices
 
 
-def add_tree(slice_, pos, meta):
-    for x in range(pos - MAX_HALF_TREE, pos + MAX_HALF_TREE + 1):
-        tree_chance = biome(x, meta)
-
-        # Set seed for random numbers based on position
-        random.seed(str(meta['seed']) + str(x) + 'tree')
-
-        # Generate a tree with a chance dependent on the biome
-        if random.random() <= tree_chance:
-            tree = random.choice(world_gen['trees'])
-
-            # Get height above ground
-            air_height = world_gen['height'] - slice_height(x, meta)
-
-            # Centre tree slice (contains trunk)
-            center_leaves = tree[int(len(tree)/2)]
-            trunk_depth = next(i for i, leaf in enumerate(center_leaves[::-1])
-                               if leaf)
-            tree_height = random.randint(2, air_height
-                          - len(center_leaves) + trunk_depth)
-
-            # Find leaves of current tree
-            for i, leaf_slice in enumerate(tree):
-                leaf_pos = x + (i - int(len(tree) / 2))
-                if leaf_pos == pos:
-                    leaf_height = air_height - tree_height - (len(leaf_slice) - trunk_depth)
-
-                    # Add leaves to slice
-                    for j, leaf in enumerate(leaf_slice):
-                        if leaf:
-                            sy = leaf_height + j
-                            slice_[sy] = spawn_hierarchy(('@', slice_[sy]))
-
-            if x == pos:
-                # Add trunk to slice
-                for i in range(air_height - tree_height,
-                               air_height):
-                    slice_[i] = spawn_hierarchy(('|', slice_[i]))
-
-    return slice_
+def spawn_hierarchy(tests):
+    # TODO: Use argument expansion for tests
+    return max(tests, key=lambda block: blocks[block]['hierarchy'])
 
 
-def biome(pos, meta):
-    biome_type = []
-
-    # Check surrounding slices for a biome marker
-    for biome_x in range(pos - int(world_gen['max_biome_size'] / 2),
-                         pos + int(world_gen['max_biome_size'] / 2)):
-        # Set seed for random numbers based on position
-        random.seed(str(meta['seed']) + str(biome_x) + 'biome')
-
-        # Generate a biome marker with a 5% chance
-        # if random.random() <= .05:
-        #     biome_type.append(random.choice(world_gen['biome_tree_weights']))
-
-    # If not plains or forest, it's normal
-    return max(set(biome_type), key=biome_type.count) if biome_type else .05
+def is_solid(block):
+    return blocks[block]['solid']
 
 
-def add_ores(slice_, pos, meta, slice_height_):
-    for ore in world_gen['ores'].values():
-        for x in range(pos - int(ore['vain_size'] / 2),
-                       pos + ceil(ore['vain_size'] / 2)):
-            # Set seed for random numbers based on position and ore
-            random.seed(str(meta['seed']) + str(x) + ore['char'])
-
-            # Generate an ore with a probability
-            if random.random() <= ore['chance']:
-                root_ore_height = random.randint(ore['lower'], ore['upper'])
-
-                # TODO: Use this for vain gen!
-                """
-                    # Generates ore at random position around root ore
-                    # TODO: Do we need a `vain_density` value per ore type?
-
-                    pot_vain_blocks = ore['vain_size'] ** 2
-                    # The bits describe the shape of the vain,
-                    #   top to bottom, left to right.
-                    vain_shape = random.getkrandbits(pot_vain_blocks)
-                    this_ores = vain_shape[]
-                """
-
-                # Generates ore at random position around root ore
-                random.seed(str(meta['seed']) + str(pos) + ore['char'])
-                ore_height = (root_ore_height +
-                              random.randint(-int(ore['vain_size'] / 2),
-                                             ceil(ore['vain_size'] / 2)))
-
-                # Won't allow ore above surface
-                if ore['lower'] < ore_height < min(ore['upper'], slice_height_):
-                    sy = world_gen['height'] - ore_height
-                    slice_[sy] = spawn_hierarchy((ore['char'], slice_[sy]))
-
-    return slice_
-
-
-def add_tall_grass(slice_, pos, meta, slice_height_):
-    # Set seed for random numbers based on position and grass
-    random.seed(str(meta['seed']) + str(pos) + 'grass')
-
-    # Generate a grass with a probability
-    if random.random() <= world_gen['tall_grass_rate']:
-        sy = world_gen['height'] - slice_height_ - 1
-        slice_[sy] = spawn_hierarchy(('v', slice_[sy]))
-
-    return slice_
+def ground_height(slice_):
+    return next(i for i, block in enumerate(slice_) if blocks[block]['solid'])
 
 
 class TerrainCache(OrderedDict):
@@ -346,6 +225,19 @@ def gen_ore_features(features, ground_heights, chunk_pos, meta):
                     features['slices'][str(x)][feature_name] = attrs
 
 
+# TODO: Convert to new terrain system
+def add_tall_grass(slice_, pos, meta, slice_height_):
+    # Set seed for random numbers based on position and grass
+    random.seed(str(meta['seed']) + str(pos) + 'grass')
+
+    # Generate a grass with a probability
+    if random.random() <= world_gen['tall_grass_rate']:
+        sy = world_gen['height'] - slice_height_ - 1
+        slice_[sy] = spawn_hierarchy(('v', slice_[sy]))
+
+    return slice_
+
+
 def gen_chunk(chunk_n, meta):
     chunk_pos = chunk_n * world_gen['chunk_size']
 
@@ -460,25 +352,3 @@ def gen_chunk(chunk_n, meta):
     log('trees gen:', trees_gen, m=1)
 
     return chunk
-
-
-def detect_edges(map_, edges):
-    slices = []
-    for pos in range(*edges):
-        if not str(pos) in map_:
-            slices.append(pos)
-
-    return slices
-
-
-def spawn_hierarchy(tests):
-    # TODO: Use argument expansion for tests
-    return max(tests, key=lambda block: blocks[block]['hierarchy'])
-
-
-def is_solid(block):
-    return blocks[block]['solid']
-
-
-def ground_height(slice_):
-    return next(i for i, block in enumerate(slice_) if blocks[block]['solid'])
