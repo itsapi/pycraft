5006b9e895774986e5393cf792a37a55ecda8f18
Geraint White <geraintwhite@gmail.com>
2016-03-31 17:08:17 +0100

    Add missing param in 1823acebdc576b384743289f7e4b552ed3d8c018

--- rendermodule.c
+++ rendermodule.c
@@ -562,7 +562,7 @@ render_c_render(PyObject *self, PyObject *args)
 
                 if (settings.terminal_output)
                 {
-                    if (terminal_out(&printable_char, x, y, width))
+                    if (terminal_out(&frame, &printable_char, x, y, width))
                         return NULL;
                 }
 
