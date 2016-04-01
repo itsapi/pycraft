238f87158ad5cf4bab7a1a057f40deac64e654ea
Geraint White <geraintwhite@gmail.com>
2016-04-01 14:41:46 +0100

    Update tester.py for LEDS

--- tester.py
+++ tester.py
@@ -2,13 +2,13 @@ import main as pycraft
 
 
 def main():
-    _, settings, _, _, _ = pycraft.setup()
+    _, settings, _, _, _, leds = pycraft.setup()
     save = pycraft.saves.new_save({'name': 'test', 'seed': 'This is a test!'})
 
     try:
-        pycraft.game(pycraft.server_interface.LocalInterface('tester', save, 0), settings)
+        pycraft.game(pycraft.server_interface.LocalInterface('tester', save, 0), settings, leds)
     finally:
-        pycraft.setdown()
+        pycraft.setdown(leds)
 
 
 if __name__ == '__main__':
