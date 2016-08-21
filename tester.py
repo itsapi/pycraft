e080265599bd4a4209b597cc9d2970361771e632
Geraint White <geraintwhite@gmail.com>
2016-08-21 21:02:14 +0100

    Fix tester.py

--- tester.py
+++ tester.py
@@ -3,6 +3,9 @@ import main as pycraft
 
 def main():
     _, settings, _, _, _, _ = pycraft.setup()
+
+    pycraft.setup_render_c(settings)
+
     save = pycraft.saves.new_save({'name': 'test', 'seed': 'This is a test!'})
 
     try:
