1def1041c7e02d06bf271283fada2efdacd6c68e
Geraint White <geraintwhite@gmail.com>
2016-12-21 09:36:31 +0000

    set default neopixels value

--- saves.py
+++ saves.py
@@ -26,6 +26,7 @@ default_settings = {
     'fancy_lights': True,
     'terminal_output': True,
     'render_c': False,
+    'neopixels': False,
     'gravity': False,
     'flight': False,
     'width': 40,
