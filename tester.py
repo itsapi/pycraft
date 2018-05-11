26e1403f3efcb206aac84e41fa20224ee203c7dc
Oliver Faircliff <faircliff.oliver@gmail.com>
2018-05-11 23:17:14 +0100

    Fix pycraft tester

--- tester.py
+++ tester.py
@@ -3,13 +3,13 @@ import main as pycraft
 
 def main():
     _, settings, _, _, benchmarks, _, _ = pycraft.setup()
+    pycraft.render_interface.setup_render_module(settings)
 
-    render_c = pycraft.import_render_c(settings)
     save = pycraft.saves.new_save({'name': 'test', 'seed': 'This is a test!'})
     server_obj = pycraft.server_interface.LocalInterface('tester', save, 0, settings)
 
     try:
-        pycraft.game(server_obj, settings, render_c, benchmarks)
+        pycraft.game(server_obj, settings, benchmarks)
     finally:
         pycraft.setdown()
 
